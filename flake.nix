28768b8db736275e0b4e7c50275f5256865cedd0
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2024-12-22 14:20:40 -0500

    add deps and code for iced cpounter example

--- flake.nix
+++ flake.nix
@@ -16,18 +16,33 @@
     (
       system: let
         overlays = [(import rust-overlay)];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
+        pkgs = import nixpkgs {inherit system overlays;};
+        lib = pkgs.lib;
 
         rustToolchain = pkgs.pkgsBuildHost.rust-bin.stable.latest.default.override {
           extensions = ["rust-src"];
         };
+
+        icedDeps = with pkgs; [
+          expat
+          fontconfig
+          freetype
+          freetype.dev
+          libGL
+          pkg-config
+          xorg.libX11
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXrandr
+          wayland
+          libxkbcommon
+        ];
       in {
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [rustToolchain alejandra];
+          buildInputs = with pkgs; [rustToolchain alejandra] ++ icedDeps;
           shellHook = ''
             export RUST_SRC_PATH =${rustToolchain}/lib/rustlib/src/rust/src
+            export LD_LIBRARY_PATH=${lib.makeLibraryPath icedDeps}:$LD_LIBRARY_PATH
           '';
         };
       }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
-fn main() {
-  println!("Hello, world!");
+use iced::widget::{button, column, text, Column};
+
+fn main() -> Result<(), iced::Error> {
+  iced::run("A cool counter", Counter::update, Counter::view)
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+  Increment,
+  Decrement,
+}
+
+#[derive(Default)]
+struct Counter {
+  value: i64,
+}
+
+impl Counter {
+  fn update(&mut self, message: Message) {
+    match message {
+      Message::Increment => {
+        self.value += 1;
+      }
+      Message::Decrement => {
+        self.value -= 1;
+      }
+    }
+  }
+
+  fn view(&self) -> Column<Message> {
+    column![
+      button("+").on_press(Message::Increment),
+      text(self.value),
+      button("-").on_press(Message::Decrement)
+    ]
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  #[test]
+  fn it_counts_properly() {
+    use super::*;
+
+    let mut counter = Counter { value: 0 };
+
+    counter.update(Message::Increment);
+    counter.update(Message::Increment);
+    counter.update(Message::Decrement);
+
+    assert_eq!(counter.value, 1);
+  }
 }
