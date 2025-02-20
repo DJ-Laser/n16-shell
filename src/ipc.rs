b58a9249ed38949e7f4e1a1e5fcfde348a9c6474
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2025-02-20 11:33:14 -0500

    add ipc fn for n16 command

---
+++ src/ipc.rs
@@ -0,0 +1,30 @@
+use std::{
+  io::{self, BufRead, BufReader, Write},
+  net::Shutdown,
+  os::unix::net::UnixStream,
+};
+
+use n16_ipc::{Reply, Request};
+
+pub fn send_request(request: Request) -> io::Result<Reply> {
+  let mut stream = UnixStream::connect(
+    n16_ipc::get_socket_path()
+      .map_err(|message| io::Error::new(io::ErrorKind::NotFound, message))?,
+  )?;
+
+  let mut buf = serde_json::to_vec(&request)?;
+  buf.push(b'\n');
+
+  stream.write_all(&buf)?;
+
+  let mut buf = String::new();
+  BufReader::new(stream.try_clone()?).read_line(&mut buf)?;
+
+  let reply = serde_json::from_str(&buf)
+    .map_err(|err| err.to_string())
+    .and_then(|op| op);
+
+  stream.shutdown(Shutdown::Both)?;
+
+  Ok(reply)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 use std::env::args;
 
+use ipc::send_request;
 use n16_ipc::{launcher, Request};
+
+mod ipc;
 
 pub fn main() {
   match args().nth(1) {
     Some(arg) if arg == "--daemon" => n16_daemon::run_daemon(),
     Some(arg) => panic!("Unexpected arg {}", arg),
     None => (),
-  }
+  };
 
-  print!("{}", Request::Launcher(launcher::Request::Open).to_json())
+  send_request(Request::Launcher(launcher::Request::Open))
+    .unwrap()
+    .unwrap();
 }
