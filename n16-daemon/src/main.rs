fea419ab0ec43403eea9e688a79b1a6bb444c41c
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2025-05-05 08:44:43 -0400

    make n16-daemon a seperate binary

---
+++ n16-daemon/src/main.rs
@@ -0,0 +1,5 @@
+use n16_daemon::run_daemon;
+
+pub fn main() {
+  run_daemon()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,11 @@
 use std::env::args;
 
 use ipc::send_request;
-use n16_ipc::{launcher, Request};
+use n16_ipc::{Request, launcher};
 
 mod ipc;
 
 pub fn main() {
-  match args().nth(1) {
-    Some(arg) if arg == "--daemon" => n16_daemon::run_daemon(),
-    Some(arg) => panic!("Unexpected arg {}", arg),
-    None => (),
-  };
-
   send_request(Request::Launcher(launcher::Request::Open))
     .unwrap()
     .unwrap();
